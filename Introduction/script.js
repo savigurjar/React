0e0a9a4a2283ddab0217cab7169932949f8b7eb0
savigurjar <savigurjar1201@gmailcom>
2025-03-11 13:18:36 +0530

    first learning

--- Introduction/script.js
+++ Introduction/script.js
@@ -1,22 +1,31 @@
 // create element throght js
 
 const react = {
-    createElement: function(tag,property,children){
+    createElement: function(tag, style, children) {
         const element = document.createElement(tag);
+        element.innerText = children;
+        return element;
     }
 }
 
-const header1 = document.createElement('h1');
-header1.innerHTML = "Hello Coder Army";
-header1.style.backgroundColor = "blue";
-header1.style.fontSize = "30px";
-header1.style.color = "white";
+const header1 = react.createElement('h1', {}, "Hello Coder Army");
+const header2 = react.createElement('h1', {}, "Hello Coders");
 
-const header2 = document.createElement('h1');
-header2.innerHTML = "Hello Coders";
-header2.style.backgroundColor = "pink";
-header2.style.fontSize = "30px";
-header2.style.color = "black";
+// console.log(header1, header2);
+
+
+
+// const header1 = document.createElement('h1');
+// header1.innerHTML = "Hello Coder Army";
+// header1.style.backgroundColor = "blue";
+// header1.style.fontSize = "30px";
+// header1.style.color = "white";
+
+// const header2 = document.createElement('h1');
+// header2.innerHTML = "Hello Coders";
+// header2.style.backgroundColor = "pink";
+// header2.style.fontSize = "30px";
+// header2.style.color = "black";
 
 const root = document.getElementById('root');
 root.append(header1);
