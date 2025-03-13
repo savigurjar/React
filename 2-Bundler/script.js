ec156030655739aab27828ed168c75db507f45e1
savigurjar <savigurjar1201@gmailcom>
2025-03-13 14:56:19 +0530

    render two elements

---
+++ 2-Bundler/script.js
@@ -0,0 +1,27 @@
+const element = React.createElement(
+  "h1",
+  {
+    id: "first",
+    className: "firstname",
+    style: { backgroundColor: "blue", fontSize: "30px", color: "pink" },
+  },
+  "Hello Coder Army"
+);
+const element2 = React.createElement(
+  "h2",
+  {
+    id: "fst",
+    className: "name",
+    style: { backgroundColor: "black", fontSize: "30px", color: "pink" },
+  },
+  "Hello Coders"
+);
+const div1 = React.createElement("div", {}, [element, element2]);
+
+// document.getElementById("root");
+// ReactDOM.render(element,root); // ---2017
+
+// 2018 -> js single threaded ->khdu control le rha react element ko
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(element);
+root.render(div1);
