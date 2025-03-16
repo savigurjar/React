5e7e669e973091d8cdd9fe2d325bb8420df4c308
savigurjar <savigurjar1201@gmailcom>
2025-03-16 16:08:36 +0530

    js expression

--- 4-mntra project/readme.md
+++ 4-mntra project/readme.md
@@ -3,3 +3,6 @@ npm install react
 npm install react-dom 
 npm install -D parcel 
 <!-- -D = dev dependencies does not include in production code -->
+
+
+// jsx : js expression -> statement nhi likh skte 
--- 4-mntra project/script.js
+++ 4-mntra project/script.js
@@ -4,3 +4,10 @@ import ReactDOM from "react-dom/client";
 const element1 = <h1>Hello Coder Army</h1>
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(element1);
+
+// jsx : js expression((output lakr de) -> statement nhi likh skte 
+function greet(name)
+{
+    return <h2>ok ji {name}</h2>
+}
+root.render(greet("savi"));
