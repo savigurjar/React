65ff853ea16accc1c9992153779215653a83cebf
savigurjar <savigurjar1201@gmailcom>
2025-03-16 15:48:13 +0530

    added root

--- 4-mntra project/script.js
+++ 4-mntra project/script.js
@@ -1,2 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+const element1 = <h1>Hello Coder Army</h1>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(element1);
