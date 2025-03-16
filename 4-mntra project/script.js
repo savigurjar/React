41ed782b21c7c544df54a0992b778822c4db634d
savigurjar <savigurjar1201@gmailcom>
2025-03-16 16:27:32 +0530

    props

--- 4-mntra project/script.js
+++ 4-mntra project/script.js
@@ -6,8 +6,17 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(element1);
 
 // jsx : js expression((output lakr de) -> statement nhi likh skte 
-function greet(name)
+// function greet(name)
+// {
+//     return <h2>ok ji {name}</h2>
+// }
+// root.render(greet("savi"));
+
+// function ke name ka first letter capital hona chahiye - user defined - jsx rules
+//props = agrument pass kr rhe ho as a object
+function Geet(props)
 {
-    return <h2>ok ji {name}</h2>
+    return <h2>byyy yrr {props.name} {props.age}</h2>
 }
-root.render(greet("savi"));
+const ele2 = <Geet name="sviii" age="20"/>
+root.render(ele2);
