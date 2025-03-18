5533768ba170107400472fcf3394d108cd54ff94
savigurjar <savigurjar1201@gmailcom>
2025-03-18 15:47:57 +0530

    utils

--- 4-mntra project/src/myntra.js
+++ 4-mntra project/src/myntra.js
@@ -3,71 +3,12 @@ import ReactDOM from "react-dom/client";
 import Card from "./component/Card";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
+import arr from "./utils/dummy";
+// import {greet as goa ,meet} from "./utils/dummy"; - rename bhi kr skte h
 
 // component or module - headerfun, footerfun
 
-const arr = [
-  {
-    frame: "Photo Frame",
-    offer: "20-70%",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdNyKW-mM5QoDFCinUaqJaa0uR8OU9-GT8UQ&s",
-  },
-  {
-    frame: "V-Frame",
-    offer: "70%",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYkH1UseyNERHGMLd4LyOru2dtYOWzDUINuQ&s",
-  },
-  {
-    frame: "BTS Frame",
-    offer: "60-70%",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6pf84FWakat6WzCqHdiF5M-bIS8gHFLUzZg&s",
-  },
-  {
-    frame: "Tae Frame",
-    offer: "30-70%",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6pf84FWakat6WzCqHdiF5M-bIS8gHFLUzZg&s",
-  },
-  {
-    frame: "Tata Frame",
-    offer: "70%",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYkH1UseyNERHGMLd4LyOru2dtYOWzDUINuQ&s",
-  },
-  {
-    frame: "Photo Frame",
-    offer: "20-70%",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdNyKW-mM5QoDFCinUaqJaa0uR8OU9-GT8UQ&s",
-  },
-  {
-    frame: "Photo Frame",
-    offer: "20-70%",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdNyKW-mM5QoDFCinUaqJaa0uR8OU9-GT8UQ&s",
-  },
-  {
-    frame: "V-Frame",
-    offer: "70%",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYkH1UseyNERHGMLd4LyOru2dtYOWzDUINuQ&s",
-  },
-  {
-    frame: "BTS Frame",
-    offer: "60-70%",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6pf84FWakat6WzCqHdiF5M-bIS8gHFLUzZg&s",
-  },
-  {
-    frame: "Tae Frame",
-    offer: "30-70%",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6pf84FWakat6WzCqHdiF5M-bIS8gHFLUzZg&s",
-  },
-];
+
 
 // footer
 
---
+++ 4-mntra project/src/utils/dummy.js
@@ -0,0 +1,63 @@
+const arr = [
+    {
+      frame: "Photo Frame",
+      offer: "20-70%",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdNyKW-mM5QoDFCinUaqJaa0uR8OU9-GT8UQ&s",
+    },
+    {
+      frame: "V-Frame",
+      offer: "70%",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYkH1UseyNERHGMLd4LyOru2dtYOWzDUINuQ&s",
+    },
+    {
+      frame: "BTS Frame",
+      offer: "60-70%",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6pf84FWakat6WzCqHdiF5M-bIS8gHFLUzZg&s",
+    },
+    {
+      frame: "Tae Frame",
+      offer: "30-70%",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6pf84FWakat6WzCqHdiF5M-bIS8gHFLUzZg&s",
+    },
+    {
+      frame: "Tata Frame",
+      offer: "70%",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYkH1UseyNERHGMLd4LyOru2dtYOWzDUINuQ&s",
+    },
+    {
+      frame: "Photo Frame",
+      offer: "20-70%",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdNyKW-mM5QoDFCinUaqJaa0uR8OU9-GT8UQ&s",
+    },
+    {
+      frame: "Photo Frame",
+      offer: "20-70%",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdNyKW-mM5QoDFCinUaqJaa0uR8OU9-GT8UQ&s",
+    },
+    {
+      frame: "V-Frame",
+      offer: "70%",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYkH1UseyNERHGMLd4LyOru2dtYOWzDUINuQ&s",
+    },
+    {
+      frame: "BTS Frame",
+      offer: "60-70%",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6pf84FWakat6WzCqHdiF5M-bIS8gHFLUzZg&s",
+    },
+    {
+      frame: "Tae Frame",
+      offer: "30-70%",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6pf84FWakat6WzCqHdiF5M-bIS8gHFLUzZg&s",
+    },
+  ];
+  export default arr;
