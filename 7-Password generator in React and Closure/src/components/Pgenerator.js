31a9c8067dba885302885664e9ad8dc9206724b4
savigurjar <savigurjar1201@gmail.com>
2025-03-22 20:53:19 +0530

    memo added

---
+++ 7-Password generator in React and Closure/src/components/Pgenerator.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+const Pgenerator = React.memo(function PasswordGenerator() {
+    const [password, setPassword] = useState("");
+    const [length, setLength] = useState(12);
+    const [numberchange, setNumber] = useState(false);
+    const [characterchange, setCharacter] = useState(false);
+  
+    function generatePsd() {
+      let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      if (numberchange) {
+        str += "0123456789";
+      }
+      if (characterchange) {
+        str += "~!@#$%^&*(){}:?|";
+      }
+  
+      let pass = "";
+      for (let i = 0; i < length; i++) {
+        pass += str[Math.floor(Math.random() * str.length)];
+      }
+      setPassword(pass);
+    }
+  
+    useEffect(() => {
+      generatePsd();
+    }, [length,numberchange,characterchange]);
+  
+    return (
+      <>
+        <h1>Password is : {password}</h1>
+        <div className="psd">
+          <input
+            type="range"
+            min={7}
+            max={30}
+            value={length}
+            onChange={(e) => setLength(e.target.value)}
+          ></input>
+          <label>Length : ({length}) </label>
+  
+          <input
+            type="checkbox"
+            defaultChecked={numberchange}
+            onChange={() => setNumber(!numberchange)}
+          ></input>
+          <label> Number </label>
+  
+          <input
+            type="checkbox"
+            defaultChecked={characterchange}
+            onChange={() => setCharacter(!characterchange)}
+          ></input>
+          <label> Character </label>
+        </div>
+      </>
+    );
+  })
+  export default Pgenerator;
