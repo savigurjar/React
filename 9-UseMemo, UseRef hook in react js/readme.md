19729b3eead912a32034f5fddd5f8b70e811f162
savigurjar <savigurjar1201@gmail.com>
2025-03-26 14:25:07 +0530

    useRef

--- 9-UseMemo, UseRef hook in react js/readme.md
+++ 9-UseMemo, UseRef hook in react js/readme.md
@@ -5,3 +5,7 @@
 recursive code- o(2^n) high complexity
 
 useMemo(()=>(),[]) callbackfunction ,dependecy array , if number change tbhi call hoga old result ko store kr ke rkhega
+
+
+useref = is a react hook that lets you reference a value that's not needed for rendering- function ko re-render nhi hoga(fir se call nhi hoga(only state value = useState can re -render), and ye old value ko hold kr ke rkhta h)
+(object deta h - use under jo value di h usko store kr ke rkhta h )
--- 9-UseMemo, UseRef hook in react js/src/userefhook.js
+++ 9-UseMemo, UseRef hook in react js/src/userefhook.js
@@ -1,2 +1,26 @@
-import React, { useState, useMemo, useEffect, useCallback } from "react";
+import React, { useState, useRef } from "react";
 import ReactDOM from "react-dom/client";
+import "../ref.css";
+function Main() {
+  const [count, setcount] = useState(0);
+  let money = useRef(0);
+  console.log(money.current); //direct access nhi kr skte q ki useref ek object dega uske under key me store rhti h value
+  return (
+    <>
+      <h1>Count is : {count}</h1>
+      <button onClick={() => setcount(count + 1)}>Increment</button>
+
+      <h1>Money is : {money.current}</h1>
+      <button
+        onClick={() => {
+          money.current = money.current + 1;
+          console.log(money);
+        }}
+      >
+        Increment
+      </button>
+    </>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Main />);
